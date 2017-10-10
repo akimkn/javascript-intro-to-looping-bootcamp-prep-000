13a0fda0c26aa90389d3a5f84d6d6856d0939d6d
Austin Kim <ask199416@gmail.com>
2017-10-10 07:09:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   var result = []
   result === [...array]
   for (let i=1, i<26; i++) {
-    result.push("I am ${i} strange loops.")
+    result.push(`I am ${i} strange loops.``)
   }
   return result
 }
